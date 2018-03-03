9bec6128a03bf2a724d0bd747dc989dfdaf62a60
Gillian Lemke <gillymail15@gmail.com>
2018-03-03 10:13:52 -0500

    add new button goes to add product form

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, url_for, redirect
 from flask import render_template
 from products import Products
 
@@ -16,3 +16,7 @@ def products():
     product_instance = Products()
     data = product_instance.get_products()
     return render_template('products.html', products=data)
+
+@app.route('/add_new_product')
+def add_new_product():
+    return render_template('add_new_product.html')

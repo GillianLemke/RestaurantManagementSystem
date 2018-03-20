44270b8d05360b8e49bfd58d5839ac3cd5793dbb
Niko Pappas <nikopappas5@gmail.comgit config --global user.name Nikogit config --global user.email nikopappas5@gmail.com>
2018-03-20 15:46:46 -0400

    Started adding content to the Ingredients.html page

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ app.config['DEBUG'] = True
 @app.route('/')
 def home(name=None):
     return render_template('home.html', name=name)
+
+@app.route('/ingredients')
+def ingredients():
+    return render_template('ingredients.html')
+
+if __name__ == "__main__":
+    app.run()

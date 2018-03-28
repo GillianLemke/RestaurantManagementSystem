da98dd4dd7e0b7940d8a34487476e791a2f6e3fd
legoniko <nikopappas5@gmail.com>
2018-03-28 11:14:42 -0400

    Deleted Restaurant_ingredients.py from feature/suppliers branch. Deleted restaurant_suppliers.py and replaced it with suppliers.py in feature/suppliers. Grabbed data from suppliers database in MySQL and displayed the contents (name, contact_name, contact_number) on webpage. Edited suppliers.html to display Name, Contact Name, and Contact Number heading along with displaying the data from MySQL database (added code under {%for supplier in suppliers%}).

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from suppliers import Suppliers
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -14,7 +15,9 @@ def ingredients():
 
 @app.route('/suppliers')
 def suppliers():
-    return render_template('suppliers.html')
+    suppliers_instance = Suppliers()
+    data = suppliers_instance.get_suppliers()
+    return render_template('suppliers.html', suppliers=data)
 
 if __name__ == "__main__":
     app.run()

9b11d601a2e8d53e3c41db0a34f5ba001e0d2dc1
legoniko <nikopappas5@gmail.com>
2018-04-03 14:31:26 -0400

    Created Add and delete page for Ingredients. Updated css for form page.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, url_for, redirect, flash
 from flask import render_template
 from ingredients import Ingredients
 
@@ -14,6 +14,30 @@ def ingredients():
     ingredients_instance = Ingredients()
     data = ingredients_instance.get_ingredients()
     return render_template('ingredients.html', ingredients=data)
+
+@app.route('/add_new_ingredient', methods=['GET', 'POST'])
+def add_new_ingredient():
+    if request.method == 'POST':
+        name = request.form['ingredient-name']
+
+        ingredients_instance = Ingredients()
+        ingredients_instance.add_ingredient(name)
+        return redirect('/ingredients')
+    else:
+        return render_template('add_new_ingredient.html')
+
+
+@app.route('/delete_ingredient', methods=['GET', 'POST'])
+def delete_ingredient():
+    if request.method == 'POST':
+        name = request.form['ingredient-name']
+
+        ingredients_instance = Ingredients()
+        ingredients_instance.delete_ingredient(name)
+        return redirect('/ingredients')
+    else:
+        return render_template('delete_ingredient.html')
+
 
 @app.route('/suppliers')
 def suppliers():

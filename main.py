30959a3e125e2156df284c58f2632985af1ed95d
Gillian Lemke <gillymail15@gmail.com>
2018-04-07 18:49:21 -0400

    add employee pages

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, url_for, redirect, flash
 from flask import render_template
 from products import Products
 from login import Login
+from employee import Employees
 
 
 app = Flask(__name__)
@@ -54,6 +55,43 @@ def delete_product():
         return redirect('/products')
     else:
         return render_template('delete_product.html')
+
+
+@app.route('/employees')
+def employees():
+    employee_instance = Employees()
+    data = employee_instance.get_employees()
+    return render_template('employees.html', employees=data)
+
+
+@app.route('/add_new_employee', methods=['GET', 'POST'])
+def add_new_employee():
+    if request.method == 'POST':
+        name = request.form['employee-name']
+        id = request.form['employee-id']
+        wage = request.form['employee-wage']
+        status = request.form['employee-status']
+        location = request.form['employee-location']
+
+        employee_instance = Employees()
+        employee_instance.add_employee(name, id, wage, status, location)
+        return redirect('/employees')
+    else:
+        return render_template('add_new_employee.html')
+
+
+@app.route('/delete_employee', methods=['GET', 'POST'])
+def delete_employee():
+    if request.method == 'POST':
+        id = request.form['employee-id']
+        print(id)
+        print(type(str(id)))
+
+        employee_instance = Employees()
+        employee_instance.delete_employee(id)
+        return redirect('/employees')
+    else:
+        return render_template('delete_employee.html')
 
 
 if __name__ == "__main__":
